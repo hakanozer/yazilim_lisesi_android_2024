92c42e1f71f0da687c7ed149a532cbe280cc2419
Hakan ÖZER <hakanozer02@gmail.com>
2024-05-07 11:33:47 +0300

    Days-2

---
+++ Days2/app/src/main/java/com/works/days_2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.works.days_2;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGotoProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnGotoProfile = findViewById(R.id.btnGotoProfile);
+        btnGotoProfile.setOnClickListener();
+
+    }
+}
